d009cb47cc7c4f32196ce2294164f08b60b2b7d9
Mark Rogalski <interesting.ones.are.already.taken@gmail.com>
2014-02-17 16:32:15 +0100

    Parse lines and combine into categories

--- gitlight_compiled.js
+++ gitlight_compiled.js
@@ -21,51 +21,55 @@ lt.plugins.gitlight.get_cwd = (function get_cwd(){var filename = new cljs.core.K
 });
 lt.plugins.gitlight.get_branch = (function get_branch(data){return cljs.core.second.call(null,clojure.string.split.call(null,data,/On branch /));
 });
-lt.plugins.gitlight.delete_inside_parens = (function delete_inside_parens(data){return clojure.string.replace.call(null,clojure.string.trim.call(null,data),/^\(.*\)$/,"");
+lt.plugins.gitlight.delete_inside_parens = (function delete_inside_parens(data){var line_with_no_hash = clojure.string.replace_first.call(null,data,/#/," ");var trimmed = clojure.string.trim.call(null,line_with_no_hash);return clojure.string.replace.call(null,[cljs.core.str(trimmed)].join(''),/^\(.*\)$/,"");
 });
-lt.plugins.gitlight.get_files = (function get_files(data){var splitted = clojure.string.split.call(null,data,/#/);return cljs.core.map.call(null,lt.plugins.gitlight.delete_inside_parens,splitted);
+lt.plugins.gitlight.get_files = (function get_files(data){var first_line_fix = [cljs.core.str("#"),cljs.core.str(data)].join('');var splitted = clojure.string.split_lines.call(null,first_line_fix);var filtered = cljs.core.filter.call(null,((function (first_line_fix,splitted){
+return (function (p1__32893_SHARP_){return p1__32893_SHARP_.startsWith("#");
+});})(first_line_fix,splitted))
+,splitted);var no_parens = cljs.core.map.call(null,lt.plugins.gitlight.delete_inside_parens,filtered);return cljs.core.filter.call(null,(function (p1__32894_SHARP_){return cljs.core.not.call(null,clojure.string.blank_QMARK_.call(null,p1__32894_SHARP_));
+}),no_parens);
 });
 lt.plugins.gitlight.git_status = (function git_status(){var dir = lt.plugins.gitlight.get_cwd.call(null);return lt.objs.proc.exec.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"command","command",1964298941),"/usr/bin/git",new cljs.core.Keyword(null,"args","args",1016906831),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"status","status",4416389988)], null),new cljs.core.Keyword(null,"cwd","cwd",1014003170),dir,new cljs.core.Keyword(null,"obj","obj",1014014057),lt.plugins.gitlight.shell_git_out], null));
 });
-lt.plugins.gitlight.git_output = (function git_output(this$){var e__8111__auto__ = crate.core.html.call(null,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",1013907515),"asdf"], null));var seq__26612_26618 = cljs.core.seq.call(null,cljs.core.partition.call(null,2,cljs.core.PersistentVector.EMPTY));var chunk__26613_26619 = null;var count__26614_26620 = 0;var i__26615_26621 = 0;while(true){
-if((i__26615_26621 < count__26614_26620))
-{var vec__26616_26622 = cljs.core._nth.call(null,chunk__26613_26619,i__26615_26621);var ev__8112__auto___26623 = cljs.core.nth.call(null,vec__26616_26622,0,null);var func__8113__auto___26624 = cljs.core.nth.call(null,vec__26616_26622,1,null);lt.util.dom.on.call(null,e__8111__auto__,ev__8112__auto___26623,func__8113__auto___26624);
+lt.plugins.gitlight.git_output = (function git_output(this$){var e__8111__auto__ = crate.core.html.call(null,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",1013907515),"asdf"], null));var seq__32901_32909 = cljs.core.seq.call(null,cljs.core.partition.call(null,2,cljs.core.PersistentVector.EMPTY));var chunk__32902_32910 = null;var count__32903_32911 = 0;var i__32904_32912 = 0;while(true){
+if((i__32904_32912 < count__32903_32911))
+{var vec__32905_32913 = cljs.core._nth.call(null,chunk__32902_32910,i__32904_32912);var ev__8112__auto___32914 = cljs.core.nth.call(null,vec__32905_32913,0,null);var func__8113__auto___32915 = cljs.core.nth.call(null,vec__32905_32913,1,null);lt.util.dom.on.call(null,e__8111__auto__,ev__8112__auto___32914,func__8113__auto___32915);
 {
-var G__26625 = seq__26612_26618;
-var G__26626 = chunk__26613_26619;
-var G__26627 = count__26614_26620;
-var G__26628 = (i__26615_26621 + 1);
-seq__26612_26618 = G__26625;
-chunk__26613_26619 = G__26626;
-count__26614_26620 = G__26627;
-i__26615_26621 = G__26628;
+var G__32916 = seq__32901_32909;
+var G__32917 = chunk__32902_32910;
+var G__32918 = count__32903_32911;
+var G__32919 = (i__32904_32912 + 1);
+seq__32901_32909 = G__32916;
+chunk__32902_32910 = G__32917;
+count__32903_32911 = G__32918;
+i__32904_32912 = G__32919;
 continue;
 }
 } else
-{var temp__4092__auto___26629 = cljs.core.seq.call(null,seq__26612_26618);if(temp__4092__auto___26629)
-{var seq__26612_26630__$1 = temp__4092__auto___26629;if(cljs.core.chunked_seq_QMARK_.call(null,seq__26612_26630__$1))
-{var c__7497__auto___26631 = cljs.core.chunk_first.call(null,seq__26612_26630__$1);{
-var G__26632 = cljs.core.chunk_rest.call(null,seq__26612_26630__$1);
-var G__26633 = c__7497__auto___26631;
-var G__26634 = cljs.core.count.call(null,c__7497__auto___26631);
-var G__26635 = 0;
-seq__26612_26618 = G__26632;
-chunk__26613_26619 = G__26633;
-count__26614_26620 = G__26634;
-i__26615_26621 = G__26635;
+{var temp__4092__auto___32920 = cljs.core.seq.call(null,seq__32901_32909);if(temp__4092__auto___32920)
+{var seq__32901_32921__$1 = temp__4092__auto___32920;if(cljs.core.chunked_seq_QMARK_.call(null,seq__32901_32921__$1))
+{var c__7497__auto___32922 = cljs.core.chunk_first.call(null,seq__32901_32921__$1);{
+var G__32923 = cljs.core.chunk_rest.call(null,seq__32901_32921__$1);
+var G__32924 = c__7497__auto___32922;
+var G__32925 = cljs.core.count.call(null,c__7497__auto___32922);
+var G__32926 = 0;
+seq__32901_32909 = G__32923;
+chunk__32902_32910 = G__32924;
+count__32903_32911 = G__32925;
+i__32904_32912 = G__32926;
 continue;
 }
 } else
-{var vec__26617_26636 = cljs.core.first.call(null,seq__26612_26630__$1);var ev__8112__auto___26637 = cljs.core.nth.call(null,vec__26617_26636,0,null);var func__8113__auto___26638 = cljs.core.nth.call(null,vec__26617_26636,1,null);lt.util.dom.on.call(null,e__8111__auto__,ev__8112__auto___26637,func__8113__auto___26638);
+{var vec__32906_32927 = cljs.core.first.call(null,seq__32901_32921__$1);var ev__8112__auto___32928 = cljs.core.nth.call(null,vec__32906_32927,0,null);var func__8113__auto___32929 = cljs.core.nth.call(null,vec__32906_32927,1,null);lt.util.dom.on.call(null,e__8111__auto__,ev__8112__auto___32928,func__8113__auto___32929);
 {
-var G__26639 = cljs.core.next.call(null,seq__26612_26630__$1);
-var G__26640 = null;
-var G__26641 = 0;
-var G__26642 = 0;
-seq__26612_26618 = G__26639;
-chunk__26613_26619 = G__26640;
-count__26614_26620 = G__26641;
-i__26615_26621 = G__26642;
+var G__32930 = cljs.core.next.call(null,seq__32901_32921__$1);
+var G__32931 = null;
+var G__32932 = 0;
+var G__32933 = 0;
+seq__32901_32909 = G__32930;
+chunk__32902_32910 = G__32931;
+count__32903_32911 = G__32932;
+i__32904_32912 = G__32933;
 continue;
 }
 }
@@ -76,10 +80,31 @@ break;
 }
 return e__8111__auto__;
 });
-lt.plugins.gitlight.mangle_data = (function mangle_data(data){var splitted = clojure.string.split.call(null,data.toString(),/#/,3);var branch_line = cljs.core.second.call(null,splitted);var splitted_by_changes = clojure.string.split.call(null,cljs.core.nth.call(null,splitted,2),"Changes");cljs.core.println.call(null,lt.plugins.gitlight.get_branch.call(null,branch_line));
-return cljs.core.println.call(null,cljs.core.rest.call(null,cljs.core.map.call(null,lt.plugins.gitlight.get_files,splitted_by_changes,"Changes")));
+lt.plugins.gitlight.split_and_swap = (function split_and_swap(filename){var vec__32908 = clojure.string.split.call(null,filename,/:/,2);var a = cljs.core.nth.call(null,vec__32908,0,null);var b = cljs.core.nth.call(null,vec__32908,1,null);return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [clojure.string.trim.call(null,b),cljs.core.keyword.call(null,a)], null);
 });
-lt.plugins.gitlight.__BEH__shell_git__DOT__out = (function __BEH__shell_git__DOT__out(obj,data){return lt.plugins.gitlight.mangle_data.call(null,data);
+lt.plugins.gitlight.split_property = (function split_property(filenames){return cljs.core.map.call(null,lt.plugins.gitlight.split_and_swap,filenames);
+});
+lt.plugins.gitlight.make_a_category = (function make_a_category(data){var cat = cljs.core.first.call(null,data);var filenames = cljs.core.rest.call(null,data);if(cljs.core._EQ_.call(null,cat,"to be committed:"))
+{return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"to_commit","to_commit",3334658797),lt.plugins.gitlight.split_property.call(null,filenames)], null);
+} else
+{if(cljs.core._EQ_.call(null,cat,"not staged for commit:"))
+{return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"not_staged","not_staged",4226785156),lt.plugins.gitlight.split_property.call(null,filenames)], null);
+} else
+{if(cljs.core._EQ_.call(null,cat,"files:"))
+{return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"untracked","untracked",1658310115),filenames], null);
+} else
+{if(new cljs.core.Keyword(null,"else","else",1017020587))
+{return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"unknown","unknown",729063356),filenames], null);
+} else
+{return null;
+}
+}
+}
+}
+});
+lt.plugins.gitlight.mangle_data = (function mangle_data(data){var splitted = clojure.string.split.call(null,data.toString(),/#/,3);var branch_line = cljs.core.second.call(null,splitted);var splitted_by_changes = clojure.string.split.call(null,cljs.core.nth.call(null,splitted,2),/Changes|Untracked/);var grouped_by_fields = cljs.core.rest.call(null,cljs.core.map.call(null,lt.plugins.gitlight.get_files,splitted_by_changes));return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [lt.plugins.gitlight.get_branch.call(null,branch_line),cljs.core.map.call(null,lt.plugins.gitlight.make_a_category,grouped_by_fields)], null);
+});
+lt.plugins.gitlight.__BEH__shell_git__DOT__out = (function __BEH__shell_git__DOT__out(obj,data){return cljs.core.println.call(null,lt.plugins.gitlight.mangle_data.call(null,data));
 });
 lt.object.behavior_STAR_.call(null,new cljs.core.Keyword("lt.plugins.gitlight","shell-git.out","lt.plugins.gitlight/shell-git.out",1423956733),new cljs.core.Keyword(null,"reaction","reaction",4441361819),lt.plugins.gitlight.__BEH__shell_git__DOT__out,new cljs.core.Keyword(null,"desc","desc",1016984067),"When git command is executed, show its out",new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"proc.out","proc.out",4302083112),null], null), null));
 lt.plugins.gitlight.shell_git_out = lt.object.create.call(null,lt.object.object_STAR_.call(null,new cljs.core.Keyword("lt.plugins.gitlight","shell-git-out","lt.plugins.gitlight/shell-git-out",1423904478),new cljs.core.Keyword(null,"tags","tags",1017456523),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"shell-git-out","shell-git-out",543653800)], null),new cljs.core.Keyword(null,"behaviors","behaviors",607554515),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword("lt.plugins.gitlight","shell-git.out","lt.plugins.gitlight/shell-git.out",1423956733)], null)));
--- src/lt/plugins/gitlight.cljs
+++ src/lt/plugins/gitlight.cljs
@@ -18,11 +18,17 @@
   (second (string/split data #"On branch ")))
 
 (defn delete-inside-parens [data]
-  (string/replace (string/trim data) #"^\(.*\)$" ""))
+  (let [line-with-no-hash (string/replace-first data #"#" " ")
+        trimmed (string/trim line-with-no-hash)]
+    (string/replace (str trimmed) #"^\(.*\)$" "")))
 
 (defn get-files [data]
-  (let [splitted (string/split data #"#")]
-    (map delete-inside-parens splitted)))
+  (let [first-line-fix (str "#" data)
+        splitted (string/split-lines first-line-fix)
+        filtered (filter #(.startsWith %1 "#") splitted)
+        no-parens (map delete-inside-parens filtered)]
+    (filter #(not (string/blank? %1)) no-parens)))
+
 
 (defn git-status []
   (let [dir (get-cwd)]
@@ -35,21 +41,40 @@
 (defui git-output [this]
   [:h1 "asdf"])
 
+(defn split-and-swap [filename]
+  (let [[a b] (string/split filename #":" 2)]
+    [(string/trim b) (keyword a)]))
+
+(defn split-property [filenames]
+  (map split-and-swap filenames))
+
+(defn make-a-category [data]
+  (let [cat (first data)
+        filenames (rest data)]
+    (cond (= cat "to be committed:")       [:to_commit (split-property filenames)]
+          (= cat "not staged for commit:") [:not_staged (split-property filenames)]
+          (= cat "files:")                 [:untracked filenames]
+          :else                            [:unknown filenames])))
+
 
 (defn mangle-data [data]
   (let [splitted (string/split (.toString data) #"#" 3)
         branch-line (second splitted)
-        splitted-by-changes (string/split (nth splitted 2) "Changes")]
-    (do
-      (println (get-branch branch-line))
-      (println (rest (map get-files splitted-by-changes "Changes")))
-      )))
+        splitted-by-changes (string/split (nth splitted 2) #"Changes|Untracked")
+        grouped-by-fields (rest (map get-files splitted-by-changes))]
+    [(get-branch branch-line) (map make-a-category grouped-by-fields)]))
+   ; (do
+   ;   (println (get-branch branch-line))
+   ;   ;(println splitted-by-changes)
+   ;   (println (rest (map get-files splitted-by-changes)))
+   ;   (println (map make-a-category grouped-by-fields))
+   ;   )))
 
 (behavior ::shell-git.out
           :desc "When git command is executed, show its out"
           :triggers #{:proc.out}
           :reaction (fn [ obj data ]
-                      (mangle-data data)))
+                      (println (mangle-data data))))
 ;(println (get-branch (second (string/split (.toString data) #"#")))))))
 ;(notifos/set-msg! (.toString data))))
 
