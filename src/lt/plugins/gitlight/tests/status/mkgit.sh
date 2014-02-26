758266835527e62a859df9104b32db3be6c5e564
Mark Rogalski <interesting.ones.are.already.taken@gmail.com>
2014-02-26 11:37:22 +0100

    Add shell script to make test git repo.

---
+++ src/lt/plugins/gitlight/tests/status/mkgit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TEST_DIR=$(mktemp -d --tmpdir=/tmp/ gitlight-testing-XXX)
+
+cd $TEST_DIR
+git init > /dev/null
+
+for i in {1..5}
+do
+	# staged
+	F=$(mktemp --tmpdir=. staged-XXX)
+	git add $F
+
+	# not-staged
+	F=$(mktemp --tmpdir=. not-staged-XXX)
+	git add $F
+
+	# unstaged
+	mktemp --tmpdir=. unstaged-XXX > /dev/null
+done
+git commit -m "test commit" > /dev/null
+
+find $TEST_DIR -iname '*staged*' -exec sh -c 'echo "testing" >> "$1"' -- {} \;
+find $TEST_DIR -iname 'staged*' -exec git add "{}" \;
+
+echo $TEST_DIR
